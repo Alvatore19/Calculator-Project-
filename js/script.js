a7c5093c62a6446109de1b378a38d6305fe92ee4
Alvatore19 <alejandro.josealvarenga@gmail.com>
2026-05-31 15:58:30 -0600

    Maquetacion finalizada y procesos logicos comenzados

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// Variables
+
+let operandoAnterior = document.querySelector("output");
+let operandoActual = document.querySelector("output");
+let operador = document.querySelector("output");
+let resultado = document.querySelector("output");
+
+// Procesos
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        result.value += button.textContent;
+    });
+});
+
+// Suma
+
+// Resta
+
+// Multiplicacion
+
+// Division
+
+// Raiz
+
+// Potencia
+
+// Pi
+
+// Funcion
+
+// Logaritmo
+
+// Logaritmo natural
+
+// Agrupacion
+
+// Integral
+
+// Seno
+
+// Coseno
+
+// Tangente
+
+// Igual
+
+// Limpiar
