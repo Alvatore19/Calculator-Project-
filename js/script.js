93e7f9e69973e520b655595243da7b14c50d077e
Alvatore19 <alejandro.josealvarenga@gmail.com>
2026-06-01 21:34:54 -0600

    Funcionalidad en botones

--- js/script.js
+++ js/script.js
@@ -1,47 +1,134 @@
 // Variables
+const pantalla = document.querySelector("output");
 
-let operandoAnterior = document.querySelector("output");
-let operandoActual = document.querySelector("output");
-let operador = document.querySelector("output");
-let resultado = document.querySelector("output");
+let operandoAnterior = "";
+let operandoActual = "";
+let operador = undefined;
+let buttons = document.querySelectorAll("button");
 
-// Procesos
+// Procesos de los botones
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
-        result.value += button.textContent;
+        // 1. Si es un número (0-9 o punto)
+        if (button.dataset.number) {
+            agregarNumero(button.dataset.number);
+        }
+
+        // 2. Si es una operación aritmética (+, -, *, /)
+        if (button.dataset.operator) {
+            agregarOperador(button.dataset.operator);
+        }
+
+        // 3. Si es una operacion algebraica (raiz, potencia, integral, etc.)
+        if (button.dataset.action) {
+            agregarOperacionAlgebraica(button.dataset.action);
+        }
+
+        // 3. Si es una acción especial (igual, limpiar, trigonometría)
+        if (button.dataset.action) {
+            switch (button.dataset.action) {
+                case "equal":
+                    calcular();
+                    break;
+                case "clear":
+                    limpiar();
+                    break;
+                case "sqrt":
+                    raiz();
+                    break;
+                case "pow":
+                    potencia();
+                    break;
+                case "integral":
+                    integral();
+                    break;
+                case "sen":
+                    seno();
+                    break;
+                case "cos":
+                    coseno();
+                    break;
+                case "tan":
+                    tangente();
+                    break;
+                case "close":
+                    parentesisCerrado();
+                    break;
+                case "+":
+                    suma();
+                    break;
+                case "subtract":
+                    resta();
+                    break;
+                case "multiply":
+                    multiplicacion();
+                    break;
+                case "divide":
+                    division();
+                    break;
+                case "ln":
+                    logaritmoNatural();
+                    break;
+                case "log":
+                    logaritmo();
+                    break;
+                case "fx":
+                    funcion();
+                    break;
+                case "pi":
+                    pi();
+                    break;
+                case "open":
+                    parentesisAbierto();
+                    break;
+            }
+        }
     });
 });
 
-// Suma
+const agregarNumero = (numero) => {
+    if (numero === "." && operandoActual.includes(".")) {
+        return;
+    }
 
-// Resta
+    if (operandoActual === "0" && numero !== ".") {
+        operandoActual = numero;
+    } else {
+        operandoActual = operandoActual + numero;
+    }
 
-// Multiplicacion
+    actualizarPantalla();
+};
 
-// Division
+const agregarOperador = (operadorPresionado) => {
+    if (operandoActual === "") {
+        operandoActual = "0";
+    }
 
-// Raiz
+    operandoAnterior = operandoAnterior + operandoActual;
+    operandoActual = operadorPresionado;
 
-// Potencia
+    actualizarPantalla();
+};
 
-// Pi
+const agregarOperacionAlgebraica = (operacionAlgebraica) => {
+    if (operandoActual === "") {
+        operandoActual = "0";
+    }
 
-// Funcion
+    operandoAnterior = operandoAnterior + operandoActual;
+    operandoActual = operacionAlgebraica;
 
-// Logaritmo
+    actualizarPantalla();
+};
 
-// Logaritmo natural
+const calcular = (resultado) => {};
 
-// Agrupacion
+const limpiar = () => {
+    operandoActual = "0";
+    actualizarPantalla();
+};
 
-// Integral
-
-// Seno
-
-// Coseno
-
-// Tangente
-
-// Igual
-
-// Limpiar
+const actualizarPantalla = () => {
+    pantalla.textContent = operandoActual;
+};
